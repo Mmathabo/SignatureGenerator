d97ab0e90f35569916f6e6bd541c77621dc196a3
Mathabo Matjiu <mathabomatjiu@Mathabos-MacBook-Pro.local>
2017-02-21 08:39:55 +0200

    adding the signature controller

--- src/main/java/za/co/emailsignature/app/controller/SignatureGeneratorController.java
+++ src/main/java/za/co/emailsignature/app/controller/SignatureGeneratorController.java
@@ -1,9 +1,23 @@
 package za.co.emailsignature.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import za.co.emailsignature.app.model.UserDetails;
+import za.co.emailsignature.app.service.SignatureGeneratorService;
 
 @Controller
+@RequestMapping(value = "/generateSignature", method= RequestMethod.POST)
 public class SignatureGeneratorController {
 
+    @Autowired
+    SignatureGeneratorService signatureGeneratorService =  new SignatureGeneratorService();
+
+
+    public void generateSignature (@RequestBody UserDetails userDetails){
+        signatureGeneratorService.writeToFile(userDetails);
+    }
 
 }
--- src/test/java/za/co/emailsignature/app/controller/EmailGeneratorControllerTest.java
+++ src/test/java/za/co/emailsignature/app/controller/EmailGeneratorControllerTest.java
@@ -1,12 +1,40 @@
 package za.co.emailsignature.app.controller;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import za.co.emailsignature.app.model.UserDetails;
+import za.co.emailsignature.app.service.SignatureGeneratorService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/application-config.xml", "classpath:spring/mvc-config.test.xml" })
 public class EmailGeneratorControllerTest {
 
-	
+    @Mock
+    SignatureGeneratorService signatureGeneratorService;
+
+    @Mock
+    UserDetails userDetails;
+
+    private MockMvc mockMvc;
+    private SignatureGeneratorController signatureGeneratorController;
+
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+        signatureGeneratorController  = new SignatureGeneratorController();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(signatureGeneratorController).build();
+    }
+
+    @Test
+    public void testShouldSendUserDetailsToService() throws Exception {
+        //TODO: finish the test
+    }
 }
